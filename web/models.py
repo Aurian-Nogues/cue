765b8ec36e511a74a9657a334fc9b40a79eb1956
Aurian Nogues <aurian.nogues@gmail.com>
2019-03-20 17:12:35 +0000

    2

--- web/models.py
+++ web/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Message(models.Model):
+    user = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    url = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
+
+    def __str__(self):
+    return f"{self.user} / {self.title} / {self.status}"
